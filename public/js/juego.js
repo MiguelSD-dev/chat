f57f299840826496ae28cefaf11071fe666d47c7
miceica <miguelceica23@gmail.com>
2024-05-27 18:40:09 +0200

    lunes3

--- public/js/juego.js
+++ public/js/juego.js
@@ -1,11 +1,22 @@
 const socket = io();
+let connectedUsers = document.getElementById("connectedUsers");
 
 socket.on('mensaje', (datos) => {
-    console.log(datos);
+    datos.forEach(user => {
+        if (!document.getElementById(user._id)) {
+            const li = document.createElement('li');
+            li.id = user._id;
+            li.textContent = user.name;
+            connectedUsers.appendChild(li);
+        }
+    });
+
+
+    //console.log(datos);
 })
 
 
-document.getElementById("btnEnviar").onclick=()=>{
-    let texto=document.getElementById("texto").value;
-    socket.emit("mensaje",texto);
+document.getElementById("btnEnviar").onclick = () => {
+    let texto = document.getElementById("texto").value;
+    socket.emit("mensaje", texto);
 }
--- server.js
+++ server.js
@@ -19,21 +19,22 @@ app.use(express.urlencoded({ extended: true }));
 // Configura el motor de plantillas EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+let usuarios=[];
 
 app.use(express.static('public'));
 
 // Configurar el middleware de sesión con connect-mongo
-app.use(session({
+const session_middleware=session({
     secret: 'mysecret', // Cambia esto por una cadena secreta segura
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
         mongoUrl: 'mongodb://localhost:27017/tresenraya',
         collectionName: 'sessions'
-    })
-}));
+    })});
+app.use(session_middleware);
 
-// Conectar a MongoDB
+// Conectar a MongoDB{_id,name}
 mongoose.connect('mongodb://localhost:27017/tresenraya')
     .then(() => {
         console.log('Conectado a MongoDB');
@@ -42,9 +43,16 @@ mongoose.connect('mongodb://localhost:27017/tresenraya')
     });
 
 //Socket
+//Socket Session
+io.use((socket,next)=>{
+    session_middleware(socket.request,socket.request.next||{},next);
+});
+
 io.on('connection', (socket) => {
         console.log("Nuevo cliente conectado" + socket.id);
-        io.emit("mensaje","Nuevo cliente conectado");
+        const {_id,name}=socket.request.session.user;
+        usuarios.push({_id,name})
+        io.emit("mensaje",usuarios);
 
 
         socket.on('disconnect',()=>{
@@ -53,7 +61,7 @@ io.on('connection', (socket) => {
 
         socket.on('mensaje', (mensaje) => {
             console.log(mensaje);
-            io.emit('mensaje', mensaje);
+            socket.broadcast.emit('mensaje', mensaje);
         })
 })
 
@@ -102,7 +110,8 @@ app.post("/register", async (req, res) => {
 })
 
 app.get("/juego", isAuthenticated,(req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'juego.html'));
+    let {_id,name}=req.session.user;
+    res.render("juego",{user:{_id,name}});
 })
 
 server.listen(PORT, () => {
